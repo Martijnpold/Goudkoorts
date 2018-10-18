00ae0025ed8c8da614fdc90da66f0e54c8bd472e
Martijn <martijntje7@gmail.com>
2018-10-18 13:49:23 +0200

    Initial upload of project files

---
+++ Goudkoorts/Controller/ViewController.cs
@@ -0,0 +1,15 @@
+﻿using Goudkoorts.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Goudkoorts.Controller
+{
+    class ViewController
+    {
+        private InputView _inputview;
+        private OutputView _outputview;
+    }
+}
