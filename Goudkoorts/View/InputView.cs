4b552751ad312b0c5d3bdc9d083fadf99796ae84
Yoran <32639287+yymar@users.noreply.github.com>
2018-10-22 14:45:44 +0200

    Add ascii stuff

--- Goudkoorts/View/InputView.cs
+++ Goudkoorts/View/InputView.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace Goudkoorts.View
 {
-    class InputView
+    public class InputView
     {
+        public ConsoleKey getKeyPress()
+        {
+            return Console.ReadKey().Key;
+        }
     }
 }
--- Goudkoorts/View/OutputView.cs
+++ Goudkoorts/View/OutputView.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Goudkoorts.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,31 @@ using System.Threading.Tasks;
 
 namespace Goudkoorts.View
 {
-    class OutputView
+    public class OutputView
     {
+
+        public void PrintMap(Map map)
+        {
+
+        }
+
+        public void WelcomeScreen()
+        {
+            System.Console.Clear();
+            Console.WriteLine("╔════════════════════════════════╦═════════════════════╗");
+            Console.WriteLine("║    Welkom Bij Goudkoorts!      ║                     ║");
+            Console.WriteLine("║                                ║                     ║");
+            Console.WriteLine("║    betekenis van de symbolen   ║  doel van het spel  ║");
+            Console.WriteLine("║                                ║                     ║");
+            Console.WriteLine("║         # : rail               ║  verzamel punten    ║");
+            Console.WriteLine("║         - : vloer              ║  door karretjes     ║");
+            Console.WriteLine("║         ~ : zee                ║  aan te laten komen ║");
+            Console.WriteLine("║         + : junction           ║  op de bestemming   ║");
+            Console.WriteLine("╚════════════════════════════════╩═════════════════════╝");
+            Console.WriteLine("");
+            Console.WriteLine("> Kies een level (1 - 6) s = stop");
+            Console.WriteLine("");  
+            Console.ReadLine();
+        }
     }
 }
